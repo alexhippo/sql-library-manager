fde3e6dd86ca80141c5a07ec0548891a8c05f162
Alex Hipolito <alexheepo@gmail.com>
2021-09-04 15:31:00 +1000

    Testing the Book model and communication with the database. Logged out the books variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Book = require("../models").Book;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET books listing. */
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  console.log(books.map(books => books.toJSON()));
+}));
 
 module.exports = router;
