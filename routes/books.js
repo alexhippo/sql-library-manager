681b46cb419c41f7816057bf75d0b7b48c609135
Alex Hipolito <alexheepo@gmail.com>
2021-09-05 15:46:27 +1000

    Added template book routes. Cleaned up index route

--- routes/books.js
+++ routes/books.js
@@ -14,11 +14,36 @@ function asyncHandler(cb) {
   }
 }
 
-/* GET books listing. */
+/* GET books listing */
 router.get('/', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
   console.log(books.map(books => books.toJSON()));
   res.render("index", { books, title: "Books" });
+}));
+
+/* GET books/new form */
+router.get('/new', asyncHandler(async (req, res) => {
+  console.log("This is where we display the form to add new books");
+}));
+
+/* POST books/new form */
+router.post('/new', asyncHandler(async (req, res) => {
+  console.log("This is where we submit the form to add new books");
+}));
+
+/* GET an individual book */
+router.get("/:id", asyncHandler(async (req, res) => {
+  console.log("This is where we retrieve and display a book by ID");
+}));
+
+/* POST an individual book */
+router.post("/:id", asyncHandler(async (req, res) => {
+  console.log("This is where we update a book's details by ID");
+}));
+
+/* POST an individual book */
+router.post("/:id/delete", asyncHandler(async (req, res) => {
+  console.log("This is where we delete a book by its ID");
 }));
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* Handler function to wrap each route. */
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next)
-    } catch (error) {
-      // Forward error to the global error handler
-      next(error);
-    }
-  }
-}
-
-/* GET books listing. */
-router.get('/', asyncHandler(async (req, res) => {
+router.get('/', (req, res, next) => {
   res.redirect('/books');
-}));
+});
 
 module.exports = router;
